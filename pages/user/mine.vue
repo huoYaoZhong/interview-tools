09eeb869b5f52050fc5464b9c783c93c4aa45f95
huoYaoZhong <1289563612@qq.com>
2025-09-12 16:25:01 +0800

    公司提交底部自定义tab

--- pages/user/mine.vue
+++ pages/user/mine.vue
@@ -1,7 +1,7 @@
 <template>
-  <view class="content">
-      <view>{{t('Home')}}</view>
-  </view>
+    <view class="content">
+        <view>我的</view>
+    </view>
 </template>
 
 <script setup lang="ts">
@@ -31,28 +31,28 @@ function aa(){
 
 <style>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 .logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
 }
 
 .text-area {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 .title {
-  font-size: 36rpx;
-  color: #8f8f94;
+    font-size: 36rpx;
+    color: #8f8f94;
 }
 </style>
